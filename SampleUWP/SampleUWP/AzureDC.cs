f3e603b0d88ddd5a744bc358babbd30b110fdceb
Ramesh Paduchuri <vepaduch@microsoft.com>
2019-05-18 16:25:24 +0530

    the first commit

---
+++ SampleUWP/SampleUWP/AzureDC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace SampleUWP
+{
+    public sealed class AzureDC
+    {
+        private async Task<MethodResponse> TestCallback(MethodRequest methodRequest, object userContext)
+        {
+            // _logger.Information($"Test Callback called.");
+            MethodResponse mr = new MethodResponse(200);
+            return mr;
+        }
+        public async void initialize()
+        {
+            
+            // TODO: change connection string here.
+            var deviceConnectionString = "<change connection string here.>";
+
+
+            var _deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString,
+                    new ITransportSettings[]
+                    {
+                        new AmqpTransportSettings(TransportType.Amqp_WebSocket_Only)
+                        {
+                            AmqpConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                            // TODO:Pooling is disabled due to issue in SDK and Windows update.
+                            //{
+                            //    Pooling = true,
+                            //    MaxPoolSize = 100
+                            //}
+                        }
+                    }
+                    );
+
+
+            //debugging
+            await _deviceClient.SetMethodHandlerAsync("TestMethod", TestCallback, null);
+        }
+    }
+}
---
+++ SampleUWP/SampleUWP/StartupTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace SampleUWP
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+
+        private AzureDC azureDC;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to perform background work
+            //
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+            _deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += TaskInstance_Canceled;
+
+            azureDC = new AzureDC();
+            azureDC.initialize();
+        }
+
+        private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            
+        }
+    }
+
+}
